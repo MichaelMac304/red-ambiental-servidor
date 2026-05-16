ab99d457d2cf1e820aacd28d2bb7849e1da726b1
MichaelMac304 <64283751+MichaelMac304@users.noreply.github.com>
2026-05-16 20:46:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+# CORS para ESP32 + navegador
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -10,13 +12,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# base de datos en memoria
 nodes = {}
 
+# ===================== API ESP32 =====================
 @app.post("/data")
-async def data(payload: dict):
+async def receive_data(payload: dict):
     nodes[payload["id"]] = payload
     return {"ok": True}
 
 @app.get("/nodes")
 def get_nodes():
     return nodes
+
+# ===================== FRONTEND =====================
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
